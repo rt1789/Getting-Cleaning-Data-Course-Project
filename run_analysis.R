2d611e3d32793e9747c48923c9ebcd2bea3caee4
rt1789 <rt1789@att.com>
2015-03-21 19:00:50 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,61 +1,69 @@
 library(plyr)
 
-#Question 1 - merge the train and test set to merge into one data set
+#Question 1 - merge the test and train data to merge into one data set resp.
 
-#Test Data
+#Read the Test Data into a table and store in a variable
+
 XTest_Data <- read.table("./Test/X_test.txt")
 YTest_Data <- read.table("Test/y_test.txt")
 Subject_Test_Data <- read.table("Test/subject_test.txt")
 
-#Train Data
+#Read the Train Data into a table and store in a variable
+
 XTrain_Data <- read.table("./Train/X_Train.txt")
 YTrain_Data <- read.table("Train/Y_Train.txt")
 Subject_Train_Data <- read.table("Train/subject_train.txt")
 
-#Merge the Train and Test data sets
+#Merge the Train and Test data sets and store in a variable
 
 XData_Set <- rbind(XTrain_Data,XTest_Data)
 YData_Set <- rbind(YTrain_Data,YTest_Data)
 Sub_Test_Set <- rbind(Subject_Train_Data,Subject_Test_Data)
 
-# Question 2 Extract only the measurements on the mean and standard deviation for each measurement 
+# Question 2 - Read the features data into a table and extract only the measurements on the mean and standard deviation for each measurement 
 
 Feature_Data <- read.table("features.txt") 
 
-# extract the columns with matching pattern of mean or std in the names 
+Mean_std_Data <- grep("(mean|std)\\(", Feature_Data[, 2])
 
-Mean_std_Data <- grep("(mean|std)\\(", Feature_Data[, 2]) 
+#head(XData_Set,n=10)
 
-head(XData_Set,n=10)
-
-# extract the columns with matching pattern of mean or std
+# extract the data using the row numbers of matching pattern of mean or std
 XData_Set <- XData_Set[, Mean_std_Data] 
 
-# rename the column names 
-names(XData_Set) <- Feature_Data[Mean_std_Data, 2] 
+# cleanup the column names by removing "()" and rename the column names of the merged data  
+names(XData_Set) <- gsub("(\\()(\\))", "", Feature_Data[Mean_std_Data, 2]) 
 
 # Question 3 Use descriptive activity names to name the activities in the data set 
-############################################################################### 
 
-Activity_Label_Data <- read.table("activity_labels.txt") 
+#read the activity labels data into a table
+Activity_Type_Data <- read.table("activity_labels.txt") 
 
-# update values with correct activity names 
-YData_Set[, 1] <- Activity_Label_Data[YData_Set[, 1], 2] 
+# update values in the Y data set with correct activity names using the activity Type data 
+YData_Set[, 1] <- Activity_Type_Data[YData_Set[, 1], 2] 
 
-# correct column name 
-names(YData_Set) <- "Activity_Type" 
 
 # Question 4 Appropriately label the data set with descriptive variable names 
 
-# correct column name 
+# update column name of the Subject test data set to reflect a more meaningful description
 names(Sub_Test_Set) <- "Subject_Num" 
 
+# update the column name of the Y Train and Test data set to reflect a more meaningful description
+names(YData_Set) <- "Activity_Type" 
+
 # merge all the data into a single data set 
-Merged_Data <- cbind(XData_Set, YData_Set, Sub_Test_Set) 
 
-# Question 5- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+Merged_Data <- cbind(XData_Set,Sub_Test_Set, YData_Set) 
+
+
+# Question 5- From the data set, creates a second independent tidy data set with the average of each variable for each activity and each subject
+
+# function to calculate the average
+mean_df <- function(x) colMeans(x[, 1:66])
  
-Tidy_Data <- ddply(Merged_Data, .(Subject_Num, Activity_Type), function(x) colMeans(x[, 1:66])) 
+#calculate the average of each variable for each activity and each subject
+Tidy_average_Data <- ddply(Merged_Data, .(Activity_Type,Subject_Num), mean_df)
 
-write.table(Tidy_Data, "Tidy_Data.txt", row.name=FALSE) 
+# create a data set as a txt file created with write.table() using row.name=FALSE
+write.table(Tidy_Data, "Tidy_average_Data.txt", row.name=FALSE) 
 
